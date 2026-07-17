41fe11360ac42033f8f889c997204471ce7bc558
coder-2712 <sharma.nidhi81@gmail.com>
2026-07-17 05:42:52 +0000

    Changes to be committed: modified:   pixelart.py

--- pixelart.py
+++ pixelart.py
@@ -5,11 +5,26 @@ def drawline(x0,y0,x1,y1):
     t.goto(x0,y0)
     t.pendown()
     t.goto(x1,y1)
-def drawrectangle(x0,y0,len,hgt):
+def drawrectangle(x0,y0,len,hgt,fillcolor):
+    t.fillcolor(fillcolor)
+    t.begin_fill()
     drawline(x0,y0,x0+len,y0)
     drawline(x0+len,y0,x0+len,y0+hgt)
     drawline(x0+len,y0+hgt,x0,y0+hgt)
     drawline(x0,y0+hgt,x0,y0)
-drawline(0,0,100,100)
-drawrectangle(60,60,60,60)
+    t.end_fill()
+def pixelcolor(jj,kk,color):
+    jj
+    kk
+    color
+xval=-230
+yval=150
+ncols=30
+nrows=30
+for jj in range(nrows):
+    for kk in range(ncols):
+        drawrectangle(xval,yval,15,15,"aliceblue")
+        xval = xval+15
+    xval=-230
+    yval=yval-15
 turtle.mainloop()
